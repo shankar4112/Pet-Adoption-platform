eb5073f20f071fab7383481ae18a0364309e8bfc
Gowri Shankar <shankar4112004@gmail.com>
2024-10-28 13:43:14 +0530

    change

--- src/components/router/Router.jsx
+++ src/components/router/Router.jsx
@@ -20,7 +20,7 @@ import MainLayout from "../../layouts/MainLayout";
 import PetListing from "../../pages/petListing/PetListing";
 import UserDashboard from "../../pages/userDashboard/UserDashboard";
 import AddPet from "../../pages/addpet/AddPet";
-import AddPetDashboard from "../../pages/addpet/addpetdashboard";
+import AddPetDashboard from "../..AddPetDashboard.jsx";
 import AdoptPet from "../../pages/petListing/petlistingComponent/AdoptPet";
 import MyAddedPets from "../../pages/userDashboard/myaddedpet/MyAddedPets";
 import AdoptionReq from "../../pages/userDashboard/adoptionReq/AdoptionReq";
